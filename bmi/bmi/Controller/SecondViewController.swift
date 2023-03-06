fc133e1b4516597ad986c4cf1b6c323a89ac7628
mohamdan <engineerm86@gmail.com>
2023-03-06 15:16:49 +0200

    link-UI-secondVC

---
+++ bmi/bmi/Controller/SecondViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.swift
+//  bmi
+//
+//  Created by mohamdan on 06/03/2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var bmiValueLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+    }
+    
+    
+}
