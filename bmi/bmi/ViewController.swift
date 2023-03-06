fca11a7e193031bcf32cf80e1beddd1925a2c85d
mohamdan <engineerm86@gmail.com>
2023-03-06 13:59:13 +0200

    add-UI-elements

---
+++ bmi/bmi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi
+//
+//  Created by mohamdan on 06/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
